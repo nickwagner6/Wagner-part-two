ae6090169210f7e173dfbd977251c98c72ae9f19
nick <nwagner566@yahoo.com>
2021-09-23 15:27:24 -0400

    part-two

--- js/script.js
+++ js/script.js
@@ -1,36 +1,33 @@
-debugger;
+
 var petName = prompt("What is your pets name?");
 var energy = 0;
 var happiness = 0;
-for (
-  var i = 0;
-  i < 6;
-  i++ // i is equal to 0, as long as i is less than 6, run again
-) {
-  var answer = prompt("feed, pet, walk");
-  if (answer == "walk") {
+for (var i = 0; i < 6; i++
+  // i is equal to 0, as long as i is less than 6, run again
+)var answer = prompt("feed, pet, walk");
+  if (answer == "walk"); {
     if (energy == 0) {
       alert("Not enough energy to enjoy a walk");
     }
   }
-  alter();
+
   break;
 
   if (answer == "feed") {
     energy = energy + 2;
     // increase energy by 2
   }
-  if (answer == "pet") {
+  else if (answer == "pet") {
     happiness = happiness + 1;
     // increase happiness by 1
   }
-  if (answer == "walk") {
+ else if (answer == "walk") {
     happiness = happiness + 2;
     // increase happiness by 2
     energy = energy - 1;
     // decrease energy by 1
   }
-}
+
 console.log(
   petName + " has " + happiness + " happiness " + " and " + energy + " energy "
 );
