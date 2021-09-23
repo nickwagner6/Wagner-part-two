1d8a7ec0f5edb52bf68c844798ef01d22a4ed221
nick <nwagner566@yahoo.com>
2021-09-23 02:45:42 -0400

    part two

---
+++ js/js/script.js
@@ -0,0 +1,37 @@
+debugger;
+var petName = prompt("What is your pets name?");
+var energy = 0;
+var happiness = 0;
+for (
+  var i = 0;
+  i < 6;
+  i++ // i is equal to 0, as long as i is less than 6, run again
+) {
+  var answer = prompt("feed, pet, walk");
+  if (answer == "walk") {
+    if (energy == 0) {
+      alert("Not enough energy to enjoy a walk");
+    }
+  }
+  alter();
+  break;
+
+  if (answer == "feed") {
+    energy = energy + 2;
+    // increase energy by 2
+  }
+  if (answer == "pet") {
+    happiness = happiness + 1;
+    // increase happiness by 1
+  }
+  if (answer == "walk") {
+    happiness = happiness + 2;
+    // increase happiness by 2
+    energy = energy - 1;
+    // decrease energy by 1
+  }
+}
+console.log(
+  petName + " has " + happiness + " happiness " + " and " + energy + " energy "
+);
+// console.log(`${petName} happiness is at ${happiness} and their energy level is at ${energy}.`)
